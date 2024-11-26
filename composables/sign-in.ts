2ba759eb45972a6fdb452fddee44a05d82463f79
Anbraten <anton@ju60.de>
2024-11-26 23:27:20 +0100

    use oauth for login

--- composables/sign-in.ts
+++ composables/sign-in.ts
@@ -1,14 +1,12 @@
 import type { Ref } from 'vue'
 
 export function useSignIn(input?: Ref<HTMLInputElement | undefined>) {
-  const singleInstanceServer = useRuntimeConfig().public.singleInstance
   const userSettings = useUserSettings()
   const users = useUsers()
-  const { t } = useI18n()
 
   const busy = ref(false)
   const error = ref(false)
-  const server = ref('')
+  const handle = ref('')
   const displayError = ref(false)
 
   async function oauth() {
@@ -21,57 +19,30 @@ export function useSignIn(input?: Ref<HTMLInputElement | undefined>) {
 
     await nextTick()
 
-    if (!singleInstanceServer && server.value)
-      server.value = server.value.split('/')[0]
-
     try {
-      let href: string
-      if (singleInstanceServer) {
-        href = await (globalThis.$fetch as any)(`/api/${publicServer.value}/login`, {
-          method: 'POST',
-          body: {
-            force_login: users.value.length > 0,
-            origin: location.origin,
-            lang: userSettings.value.language,
-          },
-        })
-        busy.value = false
-      }
-      else {
-        href = await (globalThis.$fetch as any)(`/api/${server.value || publicServer.value}/login`, {
-          method: 'POST',
-          body: {
-            force_login: users.value.some(u => u.server === server.value),
-            origin: location.origin,
-            lang: userSettings.value.language,
-          },
-        })
-      }
-      location.href = href
+      location.href = await (globalThis.$fetch as any)(`/api/${publicServer.value}/login`, {
+        method: 'POST',
+        body: {
+          handle: handle.value,
+          force_login: users.value.length > 0,
+          origin: location.origin,
+          lang: userSettings.value.language,
+        },
+      })
+      busy.value = false
     }
     catch (err) {
-      if (singleInstanceServer) {
-        console.error(err)
+      displayError.value = true
+      error.value = true
+      await nextTick()
+      input?.value?.focus()
+      await nextTick()
+      setTimeout(() => {
         busy.value = false
-        await openErrorDialog({
-          title: t('common.error'),
-          messages: [t('error.sign_in_error')],
-          close: t('action.close'),
-        })
-      }
-      else {
-        displayError.value = true
-        error.value = true
-        await nextTick()
-        input?.value?.focus()
-        await nextTick()
-        setTimeout(() => {
-          busy.value = false
-          error.value = false
-        }, 512)
-      }
+        error.value = false
+      }, 512)
     }
   }
 
-  return { busy, displayError, error, server, singleInstanceServer, oauth }
+  return { busy, displayError, error, handle, oauth }
 }
