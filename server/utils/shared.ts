3d86ae4c486298a28a14de9020fcea9732d708ca
Markus Unterwaditzer <markus-github@unterwaditzer.net>
2024-11-28 00:32:44 +0900

    fix: Add basic user-agent to all mastodon-bound requests (#2277)
    (cherry picked from commit 302da092484ff2b53a8286e118e2d3f10eb6c850)

--- server/utils/shared.ts
+++ server/utils/shared.ts
@@ -11,6 +11,7 @@ import memory from 'unstorage/drivers/memory'
 
 import vercelKVDriver from 'unstorage/drivers/vercel-kv'
 
+import { version } from '~/config/env'
 import { APP_NAME } from '~/constants'
 
 import type { AppInfo } from '~/types'
@@ -48,9 +49,14 @@ export function getRedirectURI(origin: string, server: string) {
   return `${origin}/api/${server}/oauth/${encodeURIComponent(origin)}`
 }
 
+export const defaultUserAgent = `${APP_NAME}/${version}`
+
 async function fetchAppInfo(origin: string, server: string) {
   const app: AppInfo = await $fetch(`https://${server}/api/v1/apps`, {
     method: 'POST',
+    headers: {
+      'user-agent': defaultUserAgent,
+    },
     body: {
       client_name: APP_NAME + (env !== 'release' ? ` (${env})` : ''),
       website: 'https://nimbus.town',
