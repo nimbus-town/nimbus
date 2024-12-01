4d677e2c7991241bd34ff1d1071db57f1c9f6acd
Prazdevs <hi@praz.dev>
2024-12-01 22:14:15 +0100

    feat(profile): avatar components

---
+++ components/profile/types.ts
@@ -0,0 +1,5 @@
+import type { AppBskyActorDefs } from '@atcute/client/lexicons'
+
+export type ProfileView = AppBskyActorDefs.ProfileView
+  | AppBskyActorDefs.ProfileViewBasic
+  | AppBskyActorDefs.ProfileViewDetailed
